3dd2b2bd65ccc24af8a338ac95e8843f9440ba4b
Hasibul Islam <hasib143sl@gmail.com>
2022-02-10 16:48:29 +0600

    baaper bank done

--- js/loginToSignup.js
+++ js/loginToSignup.js
@@ -1,12 +1,23 @@
 /* login form segment */
 document.getElementById('signup_here').style.cursor = 'pointer';
-document.getElementById('signup_here').addEventListener('click', function(){
+document.getElementById('signup_here').addEventListener('click', function () {
     window.open('signup.html', "DescriptiveWindowName",
-    "left=100,top=100,width=1030,height=700");
+        "left=100,top=100,width=1030,height=700");
 });
 
 /* go to bank account */
 const goToBank = document.getElementById('go__to__bank');
-goToBank.addEventListener('click', function(){
+goToBank.addEventListener('click', function () {
     window.open('banking.html', "DescriptiveWindowName", "left=0, top=0, width=1920, height=1080");
+});
+
+const loginValidator = document.getElementById('login__validator');
+loginValidator.addEventListener('keyup', function () {
+    if (parseInt(loginValidator.value.length) >= 8) {
+        // goToBank.removeAttribute('disabled');
+        goToBank.disabled = false;
+    }
+    else {
+        goToBank.setAttribute('disabled', true);
+    }
 });
--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 /* Bank account activities */
-// document.getElementById('withdraw__btn').addEventListener('click', function(){
-//     const withdrawAmountDigit = document.getElementById('withdraw').value;
-//     const withdrawAmountText = parseFloat(document.getElementById('withdraw__initial').innerText);
-//     withdrawAmountText += withdrawAmountDigit;
-//     console.log('Hello JS');
-// });
+// while withdraw money
+document.getElementById('withdraw__btn').addEventListener('click', function () {
+    const withdrawAmountDigit = parseFloat(document.getElementById('withdraw').value);
+    const withdrawAmountText = parseFloat(document.getElementById('withdraw__initial').innerText);
+    document.getElementById('withdraw__initial').innerText = (withdrawAmountDigit + withdrawAmountText);
+    document.getElementById('total__amount__initial').innerText = parseFloat(document.getElementById('total__amount__initial').innerText) - document.getElementById('withdraw').value;
+    document.getElementById('withdraw').value = '';
+});
+// while deposite money
+document.getElementById('deposite__btn').addEventListener('click', function () {
+    const withdrawAmountDigit = parseFloat(document.getElementById('deposite').value);
+    const withdrawAmountText = parseFloat(document.getElementById('deposite__initial').innerText);
+    document.getElementById('deposite__initial').innerText = (withdrawAmountDigit + withdrawAmountText);
+    document.getElementById('total__amount__initial').innerText = parseFloat(document.getElementById('total__amount__initial').innerText) + parseFloat(document.getElementById('deposite').value);
+    document.getElementById('deposite').value = '';
+});
